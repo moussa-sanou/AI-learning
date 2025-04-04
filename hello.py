3239f57a2c5449bce75c26daa3f496eafcdcbc76
samidollars <muss.junior@gmail.com>
2025-04-03 22:55:58 -0500

    Python basic

--- hello.py
+++ hello.py
@@ -1,2 +1,63 @@
 # Print hello world
 print('Hello, world')
+
+# Defining a variable
+# Integer
+x = 5
+print('this is an integer', x)
+
+# String
+name = 'Sami'
+print('this is a string' , name)
+
+# Numbers in python
+# Float
+grade = 3.2336
+print('this is a float' , grade)
+
+#Booleans
+# Defined by true and false
+
+# Data Structures
+#Lists
+# One dimensional list
+my_list = [1, 2, 3, 4]
+print('This is a list',my_list)
+
+my_list = ['list', 'of', 'strings']
+print(my_list)
+
+# List with different data types
+my_list = [1, 'list', False, []]
+print(my_list)
+
+# Multi dimensional list
+my_list = [[1,2,3], [False, True],[]]
+print(my_list)
+print(len(my_list))
+
+# Sets
+my_set = {1,2,3,4,5}
+print(my_set)
+print(len(my_set))
+
+# Tuples
+my_tuple = (1,2,3)
+print(my_tuple)
+print(len(my_list))
+
+''' Why use Tuples?
+They're efficient 
+Good for storing lots of little things, like x,y coordinates'''
+
+# Dictionaries
+my_dictionary = {'apple' : 'A red fruit',
+                 'bear' : 'A scary animal'}
+print(my_dictionary)
+print(my_dictionary['apple'])
+
+# Sets and Dictionaries
+''' Both are defined with curly brackets
+    Sets have a unique values, dictionaries have unique keys
+    The order doesn't matter '''
+
