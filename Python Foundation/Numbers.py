dcedb454719de43885515eeb39cb0fd1352ce38b
samidollars <muss.junior@gmail.com>
2025-04-06 22:43:36 -0500

    Python basic continued

---
+++ Python Foundation/Numbers.py
@@ -0,0 +1,10 @@
+'''There are three numeric types python: float, int  complex'''
+
+x = 20 /4
+print('This is a float',x)
+
+y = 10 * 10
+print('this is an integer' , y)
+
+z = 1j
+print('this is a complex number',z)
