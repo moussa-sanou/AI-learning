ffeac5c1371870c4d8cc7dbe1286340298fbbdf7
samidollars <muss.junior@gmail.com>
2025-04-08 22:36:23 -0500

    Python basic continued

--- Python Foundation/Numbers.py
+++ Python Foundation/Numbers.py
@@ -8,3 +8,27 @@ print('this is an integer' , y)
 
 z = 1j
 print('this is a complex number',z)
+
+# Boolean
+'''If an expression is True or False. You can evaluate any expression in Python, and get one of two
+ answers, True or False '''
+print()
+print('This is an example of a boolean expression')
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+    print('b is greater than a')
+else:
+    print('b is not greater than a')
+
+# String
+print('String in python')
+'''Slicing a string'''
+
+name = 'My is sami dollars'
+print('print the first character of name ', name[0])
