28154db6747c9be81d6a134463deed310ab535a8
samidollars <muss.junior@gmail.com>
2025-04-15 23:09:39 -0500

    Python basic continued

--- Python Foundation/While.py
+++ Python Foundation/While.py
@@ -1,8 +1,25 @@
-
+from datetime import datetime
 # While loop
 ''' With the while loop we can execute a set of statements as long as a condition is true '''
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1
+CurrentTime = datetime.now().second + 2
+print(CurrentTime)
+
+wait_until = (datetime.now().second + 2) % 60
+
+while datetime.now().second != wait_until:
+    pass
+print(f'We are at {wait_until} seconds!')
+
+wait_until = (datetime.now().second + 2) % 60
+while True:
+    if datetime.now().second == wait_until:
+        print(f'We are at {wait_until} seconds!')
+        break
+
+while True:
+    if datetime.now().second == wait_until:
+        continue
+    break
+print(f'We are at {wait_until} seconds!')
+
