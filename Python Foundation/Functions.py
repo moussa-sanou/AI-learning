ec1cd9225f928a152f64fd8005a4b88e27fd2df2
samidollars <muss.junior@gmail.com>
2025-04-18 22:55:03 -0500

    Python basic continued

--- Python Foundation/Functions.py
+++ Python Foundation/Functions.py
@@ -11,6 +11,7 @@ def performOperation(num1, num2, operation):
         return num1 * num2
 print(performOperation(2, 3, 'sum'))
 
+print('----------'*10)
 # Named Parameters
 def performOperation(num1, num2, operation='sum'):
     if operation == 'sum':
@@ -27,13 +28,40 @@ def performOperation(num1, num2, operation='sum'):
         return num1 * num2
 print(performOperation(2, 3, 'multiply'))
 
+print('----------'*10)
 # *arg
 def performOperation(*args):
     print(args)
 performOperation(1,2,3)
 
+print('----------'*10)
 # **kwargs
 def performOperation(*args, **kwargs):
     print(args)
     print(kwargs)
 performOperation(1,2,3, operation='sum')
+
+# locals()
+print('----------'*10)
+print('Print function using locals')
+def performOperation(num1, num2, operation='sum'):
+    print(locals())
+performOperation(1,2,operation='multiply')
+
+# Global
+print('----------'*10)
+
+message = 'some global data'
+varA = 2
+def function1(varA, varB):
+    print(varA)
+    print(message)
+    print(locals())
+
+def function2(varC, varB):
+    print(varA)
+    print(message)
+    print(locals())
+
+function1(1,2)
+function2(3,4)
