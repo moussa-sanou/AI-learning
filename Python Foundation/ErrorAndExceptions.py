f836ccbe09cd90da77d6c9681f68a5827fd1dd25
samidollars <muss.junior@gmail.com>
2025-04-24 23:12:53 -0500

    Python basic continued

--- Python Foundation/ErrorAndExceptions.py
+++ Python Foundation/ErrorAndExceptions.py
@@ -1,5 +1,7 @@
 # Errors and Exceptions
 ''' Exceptions, when used correctly, are like a secondary layer of code'''
+import time
+
 
 def causeError():
     try:
@@ -7,7 +9,7 @@ def causeError():
     except Exception as e:
         return e
 print(causeError())
-
+print('-------' * 10)
 
 def causeError():
     try:
@@ -15,9 +17,9 @@ def causeError():
     except Exception:
         print('There was some sort of error')
 causeError()
+print('-------' * 10)
 
 # Finally
-
 def causeError():
     try:
         return 1/0
@@ -26,3 +28,64 @@ def causeError():
     finally:
         print('This will always execute!')
 causeError()
+print('-------' * 10)
+
+def causeError():
+    start = time.time()
+    try:
+        time.sleep(0.5)
+        return 1/0
+    except Exception:
+        print('There was some sort of error')
+    finally:
+        print(f'Function took {time.time() - start} second to execute')
+causeError()
+print('-------' * 10)
+
+# Catching exceptions by type
+
+def causeError():
+    try:
+        return 1 + 'a'
+    except TypeError:
+        print('There was a type error!')
+    except ZeroDivisionError:
+        print('There was a zero division error!')
+    except Exception:
+        print('There was some sort of error')
+causeError()
+print('-------' * 10)
+
+# Custom Decorators
+
+def handleException(fune):
+    def wrapper():
+        try:
+            fune()
+
+        except TypeError:
+            print('There was a type error!')
+
+        except ZeroDivisionError:
+            print('There was a zero division error!')
+        except Exception:
+            print('There was some sort of error')
+
+    return wrapper
+
+@handleException
+def causeError():
+    return 1/0
+
+causeError()
+
+print('-------' * 10)
+
+# Raising Exceptions
+
+@handleException
+def raiseError(n):
+    if n == 0:
+        raise Exception()
+    print(n)
+raiseError()
