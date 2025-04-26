de3f2e535052d795dee41cc28de02e75895e7778
samidollars <muss.junior@gmail.com>
2025-04-25 22:41:29 -0500

    Python basic continued

--- Python Foundation/ErrorAndExceptions.py
+++ Python Foundation/ErrorAndExceptions.py
@@ -89,3 +89,39 @@ def raiseError(n):
         raise Exception()
     print(n)
 raiseError()
+
+print('-------' * 10)
+
+# Customer Exceptions
+'''EXCEPTION CLASSES
+- Keep code clean and organized
+- Exception classes act as documentation 
+- They separate common expected errors from issues that require developer attention'''
+
+class CustomException(Exception):
+    pass
+
+def causeError():
+    raise CustomException('You called the causeError function!')
+causeError()
+
+print('-------' * 10)
+
+# Adding Attributes
+class HttpException(Exception):
+    statusCode = None
+    message = None
+    def __init__(self):
+        super().__init__(f'Status code: {self.statusCode} and message is: {self.message}')
+
+class NotFound(HttpException):
+    statusCode = 404
+    message = 'Resource not found'
+
+class ServerError(HttpException):
+    statusCode = 500
+    message = 'The server messed up'
+
+def raiseServerError():
+    raise ServerError()
+raiseServerError()
