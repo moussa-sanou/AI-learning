0fda6673d6b3573ec44ae176dd3b845efc867120
samidollars <muss.junior@gmail.com>
2025-05-01 23:24:10 -0500

    Python basic continued

--- Python Foundation/CSV.py
+++ Python Foundation/CSV.py
@@ -13,7 +13,28 @@ with open('10_02_us.csv', 'r') as f:
     for row in reader:
         print(row)
 
+print('---------' * 10)
+
 with open('10_02_us.csv', 'r') as f:
-    reader = list[csv.reader(f, delimiter='\t')]
+    reader = list(csv.reader(f, delimiter='\t'))
     for row in reader[1:]:
         print(row)
+print('---------' * 10)
+
+# Filtering data
+with open('10_02_us.csv', 'r') as f:
+    data = csv.reader(f, delimiter='\t')
+
+primes = []
+for number in range(2, 99999):
+    for factor in range(2, int(number**0.5)):
+        if number % factor == 0:
+            break
+    else:
+        primes.append(number)
+
+# Writing
+with open('10_02_ma_prime.csv', 'w') as f:
+    writer = csv.writer(f)
+    for row in data:
+        writer.writerow([row['place name'], row['county']])
