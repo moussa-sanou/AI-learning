a13ab02ec76e266c51c4fa13bf8f29d3c5c21c86
samidollars <muss.junior@gmail.com>
2025-05-02 23:09:27 -0500

    Python basic continued

--- Python Foundation/JSON.py
+++ Python Foundation/JSON.py
@@ -1,3 +1,26 @@
+import json
+from json import JSONDecodeError, JSONEncoder
 
 # Loading JSON
-jsonString = '{"a": "apple"}'
+jsonString = '{"a": "apple", "b": "bear", "c":"cat"}'
+json.loads(jsonString)
+print(json.loads(jsonString))
+
+# Dumping JSON
+pythonDict = {"a": "apple", "b": "bear", "c":"cat"}
+json.dumps(pythonDict)
+print(json.dumps(pythonDict))
+
+# Custom JSON Decoder
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+class AnimalEncoder(JSONEncoder):
+    def default(self, o):
+        if type(o) == Animal:
+            return o.name
+        return super().default(o)
+pythonDict = {'a' : Animal('aardvark'), 'b': Animal('bear'), 'c': Animal('cat'),}
+json.dumps(pythonDict, cls=AnimalEncoder)
+print(json.dumps(pythonDict, cls=AnimalEncoder))
